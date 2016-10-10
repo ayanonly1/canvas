4f97617e470201e80399c870050c0e59a3aa3139
n0x3u5-fc <nakshatra@fusioncharts.com>
2016-10-11 02:43:46 +0530

    Added basic text rendering features

--- sample/scripts/main.js
+++ sample/scripts/main.js
@@ -1,33 +1,27 @@
 ;(function() {
-	var init = function() {
-		var config = {
-				target: 'canvas-target',
-				width: 500,
-				height: 500,
-				context: '2d'
-			},
-			pathConfig = {
-				svgStr: 'SVG path!'
-			},
-			textConfig = {
-				textStr: 'Text string!',
-				posX: 10,
-				posY: 50,
-				fontPx: 30,
-				fontFamily: 'Arial'
-			},
-			imgConfig = {
-				imgUrl: 'res/img/sample.png',
-				posX: 5,
-				posY: 5,
-				width: 200,
-				height: 200
-			};
+    var init = function() {
+        var config = {
+            target: 'canvas-target',
+            fillColor: '#DEDEDE'
+        };
+        var textConfig = {
+            textStr: 'Text string!'
+        };
+        // var pathConfig = {
+        //   svgStr: 'SVG path!'
+        // };
+        // var imgConfig = {
+        //   imgUrl: 'res/img/sample.png',
+        //   posX: 5,
+        //   posY: 5,
+        //   width: 200,
+        //   height: 200
+        // };
 
-		var canvas = new Canvas(config);
-		canvas.path(pathConfig);
-		canvas.text(textConfig);
-		canvas.image(imgConfig);
-	};
-	init();
+        var canvas = new Canvas(config);
+        canvas.text(textConfig);
+        // canvas.path(pathConfig);
+        // canvas.image(imgConfig);
+    };
+    init();
 })();
--- scripts/Canvas.js
+++ scripts/Canvas.js
@@ -4,46 +4,57 @@ goog.provide('Canvas');
  * The base constructor for the Canvas library
  * @constructor
  *
- * @param {{target: string, width: number, height: number, context: string}} config The
- * configuration object used to initialize the canvas
- *
- * @property {string} target  The ID of target element within which the canvas will be rendered
- * @property {number} width   The width of the canvas element to be rendered
- * @property {number} height  The height of the canvas element to be rendered
- * @property {string} context The rendering context of the canvas element (2d)
+ * @param {Object} config - The configuration object used to initialize the
+ *                        canvas.
+ * @param {string} config.target - The ID of target element within which the
+ *                               canvas will be rendered.
+ * @param {number} [config.width=640] - The width of the canvas element.
+ * @param {number} [config.height=360] - The height of the canvas element.
+ * @param {string} [config.fillColor=#FFFFFF] - The color of the canvas element
+ *                                            in HEX format.
  */
 window.Canvas = function(config) {
-	this.target = config.target;
-	this.width = config.width;
-	this.height = config.height;
-	this.context = '2d';
+    if (!config.target) {
+        throw Error;
+    }
+    var target = config.target;
+    var fillColor = config.fillColor || '#FFFFFF';
+    var context = '2d';
 
-	/**
-	 * The target element which will contain the canvas
-	 * @private
-	 * @type {Element}
-	 */
-	var targetElement = document.getElementById(this.target);
+    /**
+    * The width of the canvas element in pixels.
+    * @type {number}
+    */
+    this.width = config.width || 640;
+    /**
+    * The height of the canvas element in pixels.
+    * @type {number}
+    */
+    this.height = config.height || 360;
 
-	/**
-	 * The base canvas element
-	 * @private
-	 * @type {Element}
-	 */
-	var canvasElement = document.createElement('canvas');
-	canvasElement.setAttribute('id', 'qcanvas');
-	canvasElement.setAttribute('width', this.width);
-	canvasElement.setAttribute('height', this.height);
+    /**
+    * The canvas element being rendered.
+    * @type {Element}
+    */
+    this.canvasElement = document.createElement('canvas');
+    this.canvasElement.setAttribute('id', 'qcanvas');
+    this.canvasElement.setAttribute('width', this.width);
+    this.canvasElement.setAttribute('height', this.height);
 
-	/**
-	 * The base rendering context of the canvas element
-	 * @private
-	 * @type {CanvasRenderingContext2D}
-	 */
-	var ctx = canvasElement.getContext(this.context);
+    /**
+    * The base rendering context of the canvas element.
+    * @type {CanvasRenderingContext2D}
+    */
+    this.ctx = this.canvasElement.getContext(context);
+    this.ctx.fillStyle = fillColor;
+    this.ctx.fillRect(0, 0, this.canvasElement.width, this.canvasElement.height);
 
-	ctx.fillStyle = '#3d4dad';
-	ctx.fillRect(0, 0, canvasElement.width, canvasElement.height);
-
-	targetElement.appendChild(canvasElement);
+    /**
+    * The target element in which the canvas is to be rendered.
+    * @type {Element}
+    */
+    this.targetElement = document.getElementById(target);
+    this.targetElement.style.padding = 0;
+    this.targetElement.style.margin = 0;
+    this.targetElement.appendChild(this.canvasElement);
 };
