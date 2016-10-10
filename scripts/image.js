0a5526bf02b2bcc1194c1e238e5f71b586ac6c02
n0x3u5-fc <nakshatra@fusioncharts.com>
2016-10-11 02:46:24 +0530

    Updated tabs to match eslint rules (4 spaced soft tabs)

--- scripts/image.js
+++ scripts/image.js
@@ -5,12 +5,12 @@ goog.require('Canvas');
  * @param  {{imgUrl: string}} config Contains configuration options to render an image on a canvas
  */
 window.Canvas.prototype.image = function(config) {
-	/**
-	 * Contains the URL at which to find the image
-	 * @private
-	 * @type {string}
-	 */
-	var imgUrl = config.imgUrl;
+    /**
+    * Contains the URL at which to find the image
+    * @private
+    * @type {string}
+    */
+    var imgUrl = config.imgUrl;
 
-	console.info('Image URL:', imgUrl);
+    console.info('Image URL:', imgUrl);
 };
